162f52d3fe167fb0a32bfbfe29e2c07946965323
camilo-camargo <camilocamargo49@gmail.com>
2024-01-20 23:33:46 -0500

    - Add tailwind and google League Spartant font. - add Home and Login page.

---
+++ packages/web/tailwind.config.mjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		colors: {
+			"primary-50": "#FFFCFC",
+			"primary-500": "#D95D5D",
+			"dark-800": "#382222",
+		},
+		extend: {},
+	},
+	plugins: [],
+}
