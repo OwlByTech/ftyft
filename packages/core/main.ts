49f640b8bb17ece447adb5cd04840c8279d33de5
camilo-camargo <camilocamargo49@gmail.com>
2024-01-21 00:05:00 -0500

    - Add prisma - Add create user at /api/user - Add get all users at /api/users

--- packages/core/main.ts
+++ packages/core/main.ts
@@ -1,6 +1,25 @@
 import fastify from "fastify";
 
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+
 const server = fastify();
+
+server.post('/api/user', async (request, reply) => {
+  const { name, email } = request.body as any;
+  const user = await prisma.user.create({
+    data: {
+      name: name,
+      email: email,
+    },
+  })
+
+  return user;
+});
+
+server.get("/api/users", async (request, reply) => {
+  return await prisma.user.findMany();
+});
 
 server.get("/", async (request, reply) => {
   reply.type("text/html");
