e5ae9e4c1410d8e83f29141962033cab266d373f
camilo-camargo <camilocamargo49@gmail.com>
2024-01-22 22:38:56 -0500

    - Add code comments.

--- packages/web/src/pages/api/wait-list.ts
+++ packages/web/src/pages/api/wait-list.ts
@@ -1,7 +1,11 @@
 import type { APIRoute } from "astro";
 
-export const GET: APIRoute = async ({ request }) => {
+export const POST: APIRoute = async ({ request }) => {
+  // NOTE: The content of request is the type multipart/form-data
   const data = await request.formData();
+
+  // NOTE: You can access to the form data using request.formData
+
   await fetch('http://localhost:3000/api/wait-list', {
     method: "POST",
     headers: {
