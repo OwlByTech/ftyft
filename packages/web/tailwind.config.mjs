9e78567a2753e7ec731df12ac150d4cce2e6dc27
Alejandro-Suarez-Rincon <alejandrosuarez2032002@gmailcom>
2024-02-17 21:09:49 -0500

    add page web

--- packages/web/tailwind.config.mjs
+++ packages/web/tailwind.config.mjs
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {},
+    colors: {
+      "primary-50": "#FFFCFC",
+      "primary-200": "#AD9494",
+      "primary-500": "#D95D5D",
+      "primary-950": "#382222",
+    },
+  },
+  plugins: [],
+};
